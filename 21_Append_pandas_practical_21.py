4978f0503d54a6f21ae528f94770019b502b8fee
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-07-26 14:03:13 +0500

    2.4

--- 21_Append_pandas_practical_21.py
+++ 21_Append_pandas_practical_21.py
@@ -1,15 +1,27 @@
 # ********************************Apend function ***************************
 # //////pandas append function is used to append rows of other daraframe to the end of the given dataframe, and returning a new dataframe object
 
-# //////////this is the similar to join function
-# ////////but it is join the rows and in the joijn function join columns wise
+# //////////this is the same and similar to join function  
+# ////////but it is join the rows and in the join function join columns wise
+# /////// but Remember that in the append function we join the same column names
 
 
 # /////discuss about(other, ignore_index,sort)
 
 import pandas as pd
 
-df1=pd.DataFrame({'A':[1,2,3],'B':[11,12,13]})
-df2=pd.DataFrame({'C':[4,5,6],'D':[14,15,16]})
+df1=pd.DataFrame({'A':[2,1,3],'B':[12,11,13]})
+df2=pd.DataFrame({'A':[4,5,6],'B':[14,15,16]})
 
-print(df1.append(df2))
+# //////starting append function
+# print(df1.append(df2))
+# print(df2.append(df1))
+
+# /////if we want to ignore the indexes num than we use ignore_index function
+print(df1.append(df2,ignore_index=True))
+
+
+# /////if the column names is different and we want to append it than we use sort function to saving the future error
+df3=pd.DataFrame({'A':[1,2,3],'B':[11,12,13]})
+df4=pd.DataFrame({'A':[5,6],'D':[15,16]})
+print(df3.append(df4, ignore_index=True, sort=False))
---
+++ 22_pivot_table_pandas_practical_21.py
@@ -0,0 +1,8 @@
+# *****************************Pivot Table*************************
+# -------definition-------The levels in the pivot table will be stored in multiIndex objects(Hierarchical indexes) on the index and cokumns of the result DataFrame.
+
+# ---------df.pivot_table()
+# ---------discuss about(values, index, columns, aggfunc, fill_values, margins, dropna)
+
+import pandas as pd
+
